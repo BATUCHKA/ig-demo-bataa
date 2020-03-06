9ffddbfda273000061b64926cef7aa99f92d5a14
Batuchka <bataaboldbaatar1@gmail.com>
2020-03-06 16:32:51 +0800

    firebase ynzlahin umnu

---
+++ build/precache-manifest.2834a33c784fda5987b9685c0511778a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba409ef086c61e6b67f766785e40920",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8fe3fac2dbd83b5b141",
+    "url": "/static/css/2.2623327d.chunk.css"
+  },
+  {
+    "revision": "a229e41627651f0cc549",
+    "url": "/static/css/main.c14b2971.chunk.css"
+  },
+  {
+    "revision": "d8fe3fac2dbd83b5b141",
+    "url": "/static/js/2.132f0c3d.chunk.js"
+  },
+  {
+    "revision": "efed4ab2f8932934c3f07896c7741c4a",
+    "url": "/static/js/2.132f0c3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a229e41627651f0cc549",
+    "url": "/static/js/main.625c60e4.chunk.js"
+  },
+  {
+    "revision": "c2db782c2bcea42f2e0f",
+    "url": "/static/js/runtime-main.eff71a92.js"
+  },
+  {
+    "revision": "ce7c856fd666f915af2cff250fcec9f1",
+    "url": "/static/media/sprite.ce7c856f.png"
+  },
+  {
+    "revision": "1b53a370d674a51219dd72ddf06d384c",
+    "url": "/static/media/sprite2.1b53a370.png"
+  },
+  {
+    "revision": "755f0aa0ce6c5fd5165ba98534164a0d",
+    "url": "/static/media/sprite3.755f0aa0.png"
+  }
+]);
